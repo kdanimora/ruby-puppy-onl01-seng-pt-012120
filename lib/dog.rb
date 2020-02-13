6cf816c918eefdccbe0528a5ac9f38eb167b0f8f
Karen Daniela <kdaniimora@gmail.com>
2020-02-13 22:13:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all 
+   @@all
+end 
+
+def clear_all
+  @all 
+end 
+
+def print_all
+  @all 
+  puts "#{name}"
+end 
+
+
+end 
